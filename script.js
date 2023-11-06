269802dd1cf710021a5864d3d700ce70f4b062f3
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-06 08:15:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,28 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+//your code here
+let tableEl = document.getElementById('myTable');
+let priceArr = Array.from(document.querySelectorAll("td[data-ns-test]"));
+// console.log(priceArr);
+let totalPrice = 0;
+for (const priceOfEachItem of priceArr) {
+  totalPrice += Number(priceOfEachItem.textContent);
+}
+
+
+let totalRow = document.createElement('tr');
+
+let newcell = totalRow.insertCell(0);
+
+newcell.textContent = totalPrice
+
+totalRow.setAttribute('data-ns-test', 'grandTotal')
+
+
+
+
+tableEl.insertAdjacentElement('beforeend', totalRow);
   
 };
 
